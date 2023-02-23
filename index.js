e0f6ac6c784975237a02104613461c1aafec4c46
shamima <shamimayesmin144@gamil.com>
2023-02-23 19:21:17 +0600

    added filter

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion,  ObjectId } = require('mongodb');
-const {query} = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { query } = require("express");
 require("dotenv").config();
 
 const app = express();
@@ -14,94 +14,114 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ui8slz3.mongodb.net/?retryWrites=true&w=majority`;
 
 console.log(uri);
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
 
-async function run(){
-    try{
-        const addCollection = client.db("todoApp").collection("addTask");
+async function run() {
+	try {
+		const addCollection = client.db("todoApp").collection("addTask");
 
-        // add task
-        app.post('/addTask', async(req,res)=>{
-            const addTask = req.body;
-            console.log(req.body);
-            const result = await addCollection.insertOne(addTask);
-            res.send(result)
-        })
+		// add task
+		app.post("/addTask", async (req, res) => {
+			const addTask = req.body;
+			// console.log(req.body);
+			const result = await addCollection.insertOne(addTask);
+			res.send(result);
+		});
 
-        // get alll task
-        app.get('/addTask', async (req,res)=>{
-            const query = {};
-            const cursor = addCollection.find(query);
-            const task = await cursor.toArray();
-            const result = task.reverse();
-            res.send(result);
-            console.log(result);
-        })
+		// get alll task
+		app.get("/addTask", async (req, res) => {
+			const query = {};
+			const cursor = addCollection.find(query);
+			const task = await cursor.toArray();
+			const result = task.reverse();
+			res.send(result);
+			// console.log(result);
+		});
 
-        // delete task
+		// delete task
+		app.get("/addTask/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const result = await addCollection.findOne(query);
+			res.send(result);
+		});
 		app.delete("/addTask/:id", async (req, res) => {
 			const id = req.params.id;
 			const query = { _id: new ObjectId(id) };
 			const result = await addCollection.deleteOne(query);
 			res.send(result);
+		});	
+
+		//get specific task to edit
+		app.get("/edit/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = {
+				_id: new ObjectId(id),
+			};
+			const result = await addCollection.findOne(query);
+			res.send(result);
 		});
 
-        //get specific task to update
-        app.get('/edit/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = {
-                _id: new ObjectId(id)
-            };
-            const result = await addCollection.findOne(query);
-            res.send(result);
-        })
-
-        // update task  :
+		// edit task  :
 		app.put("/edit/:id", async (req, res) => {
 			const id = req.params.id;
 			console.log(id);
 			console.log(req.body);
 			const editTitle = req.body.updatedTitle;
-            const editMessage = req.body.updatedMessage
-			const query = { _id: new ObjectId(id)};
-			const option = {upsert : true}
+			const editMessage = req.body.updatedMessage;
+			const query = { _id: new ObjectId(id) };
+			const option = { upsert: true };
 			const updatedDoc = {
 				$set: {
-                    title: editTitle,
-					message: editMessage
+					title: editTitle,
+					message: editMessage,
 				},
 			};
-			const result = await addCollection.updateOne(query, updatedDoc,option);
+			const result = await addCollection.updateOne(query, updatedDoc, option);
 			res.send(result);
 		});
 
-
-        //update field for advertise
-        app.put('/addTask/:id', async (req, res) => {
-            const id = req.params.id;
-            const filter = {
-                _id: new ObjectId(id)
-            }
-            const options = { upsert: true };
-            const updatedDoc = {
-                $set: {
-                    complete: false
-                }
-            }
-            const result = await addCollection.updateOne(filter, updatedDoc, options);
-            res.send(result);
-        })
-    }
-    finally{
-
-    }
+		//======================================================
+		//get task for update status
+		app.get("/complete", async (req, res) => {
+			const query = {
+				complete: "true",
+				status: "active",
+			};
+			const result = await addCollection.findOne(query); //.toArray();
+			res.send(result);
+		});
+		//update field for task
+		app.put("/addTask/:id", async (req, res) => {
+			const id = req.params.id;
+			const filter = {
+				_id: new ObjectId(id),
+			};
+			console.log(filter);
+			const options = { upsert: true };
+			const updatedDoc = {
+				$set: {
+					complete: "true",
+					// status : 'active'
+				},
+			};
+			const result = await addCollection.updateOne(filter, updatedDoc, options);
+			res.send(result);
+		});
+		
+	} finally {
+	}
 }
-run().catch((err) =>console.log(err));
+run().catch((err) => console.log(err));
 
-app.get('/', (req,res) =>{
-    res.send('add task is running')
-})
+app.get("/", (req, res) => {
+	res.send("add task is running");
+});
 
-app.listen(port,()=>{
-    console.log(`add task server is running on ${port}`);
-})
+app.listen(port, () => {
+	console.log(`add task server is running on ${port}`);
+});
